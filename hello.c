5428a46de6daad59fd3812c87160d7d143475ebe
Kendry Arrazcaeta <karrazcaetaduray2@huskers.unl.edu>
2021-08-25 20:38:41 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Kendry Arrazcaeta
+ *
+ * Date: 2021/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kendry Arrazcaeta\n");
+
+  return 0;
+}
